ac8d0e243eb90dad864a6d4195761e125ad7c51d
LenaAshqar <s202282660@kfupm.edu.sa>
2024-09-22 18:12:40 +0300

    method for creating random array of students

--- src/App.java
+++ src/App.java
@@ -3,4 +3,20 @@ public class App {
         System.out.println("Hello, World!");
         System.out.println("Hello Nermin");
     }
+
+    
+    public static void readStudents(String[] students) {
+        for (int i = 0; i < students.length ; i++) {
+            System.out.println(students[i]);
+        }
+    }
+
+    public static String[] createRandomStudents() {
+        int length = (int) (Math.random()*10);
+        String[] students = new String[length];
+        for (int i=0; i < length ; i++) {
+            students[i] = "Student " + (int) (Math.random()*200);
+        }
+        return students;
+    }
 }
