38ad71cd5fb97d054e73664f99b992019812a049
LenaAshqar <s202282660@kfupm.edu.sa>
2024-09-22 18:00:17 +0300

    method to read students

--- src/App.java
+++ src/App.java
@@ -3,4 +3,10 @@ public class App {
         System.out.println("Hello, World!");
         System.out.println("Hello Nermin");
     }
+
+    public static void readStudents(<String> students) {
+        for (i=0; i < students.length; i++) {
+            System.out.println(students[i]);
+        }
+    }
 }
